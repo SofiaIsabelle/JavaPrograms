9a51077919ccd4c3a89ee0c3767c827dcf0aba1a
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-09 15:49:15 -0400

    Keywords and Expressions

---
+++ KeywordsAndExpressions/src/com/sofiaisabelle/Main.java
@@ -0,0 +1,24 @@
+package com.sofiaisabelle;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// a mile is equal to 1.609344 kilometers
+        double kilometers = (100 * 1.609344);
+        int highScore = 50;
+
+        if(highScore == 50){
+            System.out.println("This is an expression.");
+
+
+
+            int score = 100; // score = 100 is an expression, the datatype and semicolon is not an expression
+            if(score > 99) // score > 99 is an expression
+                System.out.println("You got a high score!"); // You got a high score is an expression
+                score = 0; // score = 0 , is an expression as well
+        }
+    }
+}
+
+
+
