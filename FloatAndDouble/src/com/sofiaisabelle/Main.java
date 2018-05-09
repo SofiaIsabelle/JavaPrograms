08fc36628618687d278889baf94ff2bbc4e369f5
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-08 22:31:53 -0400

    Java Data-Types Float and Double

---
+++ FloatAndDouble/src/com/sofiaisabelle/Main.java
@@ -0,0 +1,35 @@
+package com.sofiaisabelle;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        int myIntValue = 5;
+        float myFloatValue = 5f / 2f;
+        double myDoubleValue = 5d / 3d;
+        System.out.println("myIntValue = " + myIntValue );
+        System.out.println("myFloatValue = " + myFloatValue);
+        System.out.println("myDoubleValue = " + myDoubleValue);
+
+
+
+
+        double numPounds = 200d;
+        double convertedKilograms = numPounds * 0.45359237d;
+        System.out.println("Kilograms = " + convertedKilograms );
+
+        double num = 3_00_00.141_69827d
+    }
+}
+
+/* Challenge :
+
+Convert a give number of pounds to kilograms
+  1. Create a variable to store the number of pounds
+  2. Calculate the number of kilograms for the number above and store it in a variable
+  3. Print out the result
+  *** Notes : 1 pound is equal to 0.45359237 kilograms
+
+
+*/
--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,6 +1,15 @@
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello, Sofia!");
+        int myFirstNumber = (3 * 11) + (57 + 10);
+        int mySecondNumber = 25;
+        int myThirdNumber = 50;
+        int myFourthNumber = 15;
+        int myFifthNumber = 10;
+        int mySixthNumber = myFirstNumber * 2;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber + myFourthNumber + myFifthNumber + mySixthNumber;
+        System.out.println(myTotal);
     }
 }
 
+/* sout : type ' sout ' , then hit tab. This will automatically type out System.out.println(); for you */
