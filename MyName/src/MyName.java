092cd3c50fd7fbece4aa54fe97915b276c82c110
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-11 09:17:54 -0400

    MyName Class

---
+++ MyName/src/MyName.java
@@ -0,0 +1,7 @@
+public class MyName {
+    public static void main(String[] args) {
+        System.out.println("My name is Sofia Isabelle ");
+        System.out.println("I am 15 years old");
+        System.out.println("My hometown is San Francisco, CA");
+    }
+}
