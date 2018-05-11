e78e50c31b586b3a6004df159d3369403a0d228d
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-11 11:35:53 -0400

    Pushing all new changes / untracked files onto Github

--- CharAndBoolean/src/com/sofiaisabelle/Main.java
+++ CharAndBoolean/src/com/sofiaisabelle/Main.java
@@ -14,6 +14,7 @@ public class Main {
         //1. Find the code for the registered symbol on the same line as the copyright symbol.
         //2. Create a variable of the type char and assign it the unicode value for that symbol
         //3. Display it on the screen
+        //4. Link for unicode symbols : https://unicode-table.com/en/#basic-latin
 
         char regChar = '\u00AE';
         System.out.println("Unicode symbol for register is " + regChar);
---
+++ HTML/HelloWorldApplet.java
@@ -0,0 +1,20 @@
+import java.awt.Graphics;
+
+//class HelloWorldApplet extends java.applet.Applet {
+//    public void paint(Graphics g) {
+//        g.drawString("Hello World !" , 5, 25);
+//    }
+//
+//}
+
+
+import java.awt.Graphics;
+
+         class HelloWorldApplet extends java.applet.Applet{
+
+         public void paint(Graphics g) {
+         g.drawString("Hello world!", 5, 25);
+
+             }
+
+         }
