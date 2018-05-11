0bf63338ac4d173cb83814d5f9fa7719e0ef2bb4
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-11 13:53:04 -0400

    Writing Methods in Java

---
+++ MealTime/src/MealTime.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MealTime {
+    public static void main(String[] args) {
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+            System.out.println("What did you eat for breakfast ?");
+            String yourBreakfast = bufferedReader.readLine();
+            System.out.println("You had " + yourBreakfast + " for breakfast.");
+
+            System.out.println("What did you have for lunch ?");
+            String yourLunch = bufferedReader.readLine();
+            System.out.println("You had " + yourLunch + " for lunch.");
+
+            System.out.println("What did you have for dinner ?");
+            String yourDinner = bufferedReader.readLine();
+            System.out.println("You had " + yourDinner + " for dinner.");
+
+
+
+        }
+        catch(IOException e)
+        {
+            e.printStackTrace();
+        }
+
+    }
+}
