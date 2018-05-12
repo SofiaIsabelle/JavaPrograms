3d7a17a5ee63c75dbfd8f83bb85ad8b14a6aaf00
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-11 20:39:45 -0400

    DRYed version of the previous code with custom methods

--- MealTime/src/MealTime.java
+++ MealTime/src/MealTime.java
@@ -3,35 +3,24 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class MealTime {
+
     public static void main(String[] args) {
+        askWhatYouAteFor("breakfast");
+        askWhatYouAteFor("lunch");
+        askWhatYouAteFor("dinner");
+    }
+
+    public static void askWhatYouAteFor(String meal){
         try{
             BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("What did you eat for " + meal + " ?") ;
+            String yourMeal = bufferedReader.readLine();
+            System.out.println("You had " + yourMeal + " for " + meal + ".");
 
-            System.out.println("What did you eat for breakfast ?");
-            String yourBreakfast = bufferedReader.readLine();
-            System.out.println("You had " + yourBreakfast + " for breakfast.");
-
-            System.out.println("What did you have for lunch ?");
-            String yourLunch = bufferedReader.readLine();
-            System.out.println("You had " + yourLunch + " for lunch.");
-
-            System.out.println("What did you have for dinner ?");
-            String yourDinner = bufferedReader.readLine();
-            System.out.println("You had " + yourDinner + " for dinner.");
-
-
-
-        }
-        catch(IOException e)
-        {
+        } catch(IOException e){
             e.printStackTrace();
         }
 
-
     }
 
-   /* public static void askWhatYouAteFor(String meal) {
-
-        System.out.println("What did you eat for " + meal + " ?");
-    }*/
 }
