db8ecc8396dd68c67144e8c43f528997a5fa48ca
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-13 07:57:35 -0400

    This is a For Loop in Java

---
+++ ShoppingList/src/ShoppingList.java
@@ -0,0 +1,9 @@
+public class ShoppingList {
+    public static void main(String[] args) {
+        String[] x = {"uggs", "dresses", "business attire", "heels"};
+
+        for(Integer index = 0; index < x.length; index++){
+            System.out.println(x[index]);
+        }
+    }
+}
