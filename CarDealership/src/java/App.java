a60344ff1b7ad6bd64ec4fe74a4eac8825a464ce
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-13 18:47:38 -0400

    Fixed and added constructor in Vehicle.java file, necesssary to make code more DRY and have the App.java lines 10 - 14 retrieve its information from.

--- CarDealership/src/java/App.java
+++ CarDealership/src/java/App.java
@@ -5,75 +5,52 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class App {
+    public static void main(String[] args) {
 
-public static void main(String[]args) {
-    BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    System.out.println("What is your maximum budget for a vehicle ?");
-
-    try {
-        String stringUserMaxBudget = bufferedReader.readLine();
-
-        Vehicle hatchback = new Vehicle();
-        hatchback.year = 1994;
-        hatchback.brand = "Subaru";
-        hatchback.model = "Legacy";
-        hatchback.miles = 170000;
-        hatchback.price = 4000;
-
-        Vehicle suv = new Vehicle();
-        suv.year = 2002;
-        suv.brand = "Ford";
-        suv.model = "Explorer";
-        suv.miles = 100000;
-        suv.price = 7000;
-
-        Vehicle sedan = new Vehicle();
-        sedan.year = 2015;
-        sedan.brand = "Toyota";
-        sedan.model = "Camry";
-        sedan.miles = 50000;
-        sedan.price = 30000;
-
-        Vehicle truck = new Vehicle();
-        truck.year = 1999;
-        truck.brand = "Ford";
-        truck.model = "Ranger";
-        truck.miles = 10000;
-        truck.price = 4000;
-
-
-        Vehicle crossover = new Vehicle();
-        crossover.year = 1998;
-        crossover.brand = "Toyota";
-        crossover.model = "Rav-4";
-        crossover.miles = 200000;
-        crossover.price = 3500;
+        Vehicle hatchback = new Vehicle(1994, "Subaru", "Legacy", 170000, 4000);
+        Vehicle suv = new Vehicle(2002, "Ford", "Explorer", 100000, 7000);
+        Vehicle sedan = new Vehicle(2015, "Toyota", "Camry", 50000, 30000);
+        Vehicle truck = new Vehicle(1999, "Ford", "Ranger", 100000, 4000);
+        Vehicle crossover = new Vehicle(1998, "Toyota", "Rav-4", 200000, 3500);
 
         Vehicle[] allVehicles = {hatchback, suv, sedan, truck, crossover};
 
-        int userMaxBudget = Integer.parseInt(stringUserMaxBudget);
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Welcome to our car dealership. What would you like to do? Enter one of the following options: All Vehicles or Search Price");
 
-        System.out.println("Alright, here's what we have within your price range: ");
+        try {
 
-        for(Vehicle individualVehicle : allVehicles){
-            if(individualVehicle.worthBuying(userMaxBudget)){
-                System.out.println("------------------------");
-                System.out.println(individualVehicle.year);
-                System.out.println(individualVehicle.brand);
-                System.out.println(individualVehicle.model);
-                System.out.println(individualVehicle.miles);
-                System.out.println(individualVehicle.price);
+            String navigationChoice = bufferedReader.readLine();
 
+            if (navigationChoice.equals("All Vehicles")) {
+                for (Vehicle individualVehicle : allVehicles) {
+                    System.out.println("----------------------");
+                    System.out.println(individualVehicle.year);
+                    System.out.println(individualVehicle.brand);
+                    System.out.println(individualVehicle.model);
+                    System.out.println(individualVehicle.miles);
+                    System.out.println(individualVehicle.price);
+                }
+            } else if (navigationChoice.equals("Search Price")) {
+                System.out.println("What is your maximum budget for a vehicle?");
+                String stringUserMaxBudget = bufferedReader.readLine();
+                int userMaxBudget = Integer.parseInt(stringUserMaxBudget);
+                System.out.println("Alright, here's what we have in your price range:");
+                for (Vehicle individualVehicle : allVehicles) {
+                    if (individualVehicle.worthBuying(userMaxBudget)) {
+                        System.out.println("----------------------");
+                        System.out.println(individualVehicle.year);
+                        System.out.println(individualVehicle.brand);
+                        System.out.println(individualVehicle.model);
+                        System.out.println(individualVehicle.miles);
+                        System.out.println(individualVehicle.price);
+                    }
+                }
+            } else {
+                System.out.println("I'm sorry, we don't recognize your input");
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-
-
     }
-        catch (IOException e) {
-        e.printStackTrace();
-
-
-       }
-    }
-
-  }
+}
--- CarDealership/src/models/Vehicle.java
+++ CarDealership/src/models/Vehicle.java
@@ -1,19 +1,27 @@
 package models;
+/* Adding a Method : a method is a function that is part of an objects blue print
+Everything in Java except primitives is an object. Each of these objects are instances of a class. */
+
+
 
 public class Vehicle {
+
     public int year;
     public String brand;
     public String model;
     public int miles;
     public int price;
 
-    /* Adding a Method : a method is a function that is part of an objects blue print */
-    public boolean worthBuying(int maxPrice){
-        return(price < maxPrice); /* define a method called worthBuying() within our Vehicle class. */
+    public Vehicle(int year, String brand, String model, int miles, int price) {
+        this.year = year;
+        this.brand = brand;
+        this.model = model;
+        this.miles = miles;
+        this.price = price;
     }
 
+    public boolean worthBuying(int maxPrice){
+        return (this.price < maxPrice);
+    }
 
 }
-
-
-/* Everything in Java except primitives is an object. Each of these objects are instances of a class. */
