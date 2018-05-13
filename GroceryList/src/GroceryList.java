dbc7b8a907ff1b028d2f24a28db338d93d8eb754
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-13 07:50:54 -0400

    For each Loop in Java

---
+++ GroceryList/src/GroceryList.java
@@ -0,0 +1,12 @@
+/* This is a for each in Java. It just used the keyword for */
+
+public class GroceryList {
+    public static void main(String[] args) {
+        String[] myGroceryList = {"greek yogurt", "apples", "turkey", "spinach", "lettuce", "mayonnaise"};
+
+        for(String x : myGroceryList){
+            System.out.println(x);
+        }
+    }
+
+}
