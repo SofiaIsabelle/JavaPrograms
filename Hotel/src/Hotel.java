2338a9ef6f856d274a60f85f9d1d81a1189fb9f4
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-12 21:59:56 -0400

    Begin by collection information about what season and time of the week the user will be traveling in.

---
+++ Hotel/src/Hotel.java
@@ -0,0 +1,32 @@
+import javafx.beans.binding.When;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Hotel {
+    public static void main(String[] args) {
+        try{
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("In what season are you booking a stay ?");
+            String season = bufferedReader.readLine();
+
+            System.out.println("On the weekend, or a weeknight?");
+            String dayOfWeek = bufferedReader.readLine();
+
+            boolean summer = season.equals("summer");
+            boolean weekend = dayOfWeek.equals("weekend");
+
+
+
+
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+}
+
+
+/*  When should I use a wrapper class over a primitive type ?
---
+++ Hotel/src/com/sofiaisabelle/Main.java
@@ -0,0 +1,8 @@
+package com.sofiaisabelle;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
