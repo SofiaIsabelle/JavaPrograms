2e09f8ff2f6782465f6aab997066998cf32e62a1
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-14 19:06:10 -0400

    Notes

--- TriangleTracker/src/TriangleTracker.java
+++ TriangleTracker/src/TriangleTracker.java
@@ -1,21 +1,25 @@
 import java.util.*;
-
+/* Create a Triangle Class */
 public class TriangleTracker{
 
     public static void main(String[] args) {
-
+/* Write a command line interface that allows users to input lengths for the 3 sides. */
         Scanner sc = new Scanner(System.in);
         int s1 = 0, s2 = 0, s3 = 0;
         System.out.println("Enter the sides of a triangle : ");
         s1 = sc.nextInt();
         s2 = sc.nextInt();
         s3 = sc.nextInt();
+/* Equilateral: All sides are equal */
         if (s1 == s2 && s2 == s3) {
             System.out.println("Triangle is Equilateral");
+/* Scalene: No sides are equal. */
         } else if (s1 != s2 && s2 != s3 && s1 != s3) {
             System.out.println("Triangle is Scalene");
 
+/*Isosceles: Exactly 2 sides are equal: (only other option)*/
         } else {
+
             System.out.println("Triangle is Isosceles");
         }
     }
