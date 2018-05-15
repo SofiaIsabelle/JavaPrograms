9c5928bde515a2e1fdccd8abbdfccb10026f399c
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-15 16:19:06 -0400

    YES! In Album.java replaced : this.genre == userPick with userPick.equals(this.genre , and now my music-shop app is working :)

--- music-shop/src/java/App.java
+++ music-shop/src/java/App.java
@@ -2,66 +2,56 @@ import models.Album;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
+
 
 public class App {
     public static void main(String[] args) {
 
+        Album latin = new Album("latin", "Shakira", "Laundry Service", 2001, 22);
+        Album rock = new Album("rock", "Green Day", "American Idiot", 2004, 25);
+        Album rap = new Album("rap", "Eminem", "Encore", 2002, 20);
+        Album pop = new Album("pop", "The Beatles", "Help!", 1995, 15);
+
+        Album[] allAlbums = {latin, rock, rap, pop};
+
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Which genre of music would you be interested in ?");
+        System.out.println("Which genre of music would you be interested in? Enter one of the following options: All Albums or Genre");
 
 
         try {
 
-            String stringUserMusicPick = bufferedReader.readLine();
+            String navigationChoice = bufferedReader.readLine();
 
-            Album latin = new Album();
-            latin.genre = "latin";
-            latin.artist = "Shakira";
-            latin.title = "Laundry Service";
-            latin.release_year = 2001;
-            latin.price = 22;
-
-
-            Album rock = new Album();
-            rock.genre = "rock";
-            rock.artist = "Green Day";
-            rock.title = "American Idiot";
-            rock.release_year = 2004;
-            rock.price = 25;
-
-
-            Album rap = new Album();
-            rap.genre = "rap";
-            rap.artist = "Eminem";
-            rap.title = "Encore";
-            rap.release_year = 2004;
-            rap.price = 20;
-
-
-            Album pop = new Album();
-            pop.genre = "pop";
-            pop.artist = "The Beatles";
-            pop.title = "Help";
-            pop.release_year = 1965;
-            pop.price = 15;
-
-
-            Album[] allAlbums = {latin, rock, rap, pop};
-            String userMusicPick = stringUserMusicPick;
-            System.out.println("Here is the album that we have for the genre of your choice :  ");
-
-            for (Album individualAlbum : allAlbums) {
-               /* if (individualAlbum.userMusicPick(userMusicPick)) { */
-                if (individualAlbum.musicPick(userMusicPick)) {
-                    System.out.println("------------------------------");
+            if (navigationChoice.equals("All Albums")) {
+                for (Album individualAlbum : allAlbums) {
+                    System.out.println("---------------------------");
                     System.out.println(individualAlbum.genre);
                     System.out.println(individualAlbum.artist);
                     System.out.println(individualAlbum.title);
                     System.out.println(individualAlbum.release_year);
                     System.out.println(individualAlbum.price);
-
                 }
+            } else if (navigationChoice.equals("Genre")) {
+                
+                System.out.println("Which genre of music is your pick : latin, rock, pop, or rap ?");
+                String stringGenreOfChoice = bufferedReader.readLine();
+                String userGenreOfChoice = stringGenreOfChoice;
+
+                System.out.println("Alright, here's what we have for your genre of choice :");
+                for (Album individualAlbum : allAlbums) {
+                    if (individualAlbum.musicPick(userGenreOfChoice)) {
+                        System.out.println("-------------------------------");
+                        System.out.println(individualAlbum.genre);
+                        System.out.println(individualAlbum.artist);
+                        System.out.println(individualAlbum.title);
+                        System.out.println(individualAlbum.release_year);
+                        System.out.println(individualAlbum.price);
+                    }
+                }
+
+
+            } else {
+                System.out.println("I'm sorry , we don't recognize your input.");
             }
 
         } catch (IOException e) {
@@ -70,5 +60,7 @@ public class App {
         }
 
     }
+
 }
+
 
--- music-shop/src/models/Album.java
+++ music-shop/src/models/Album.java
@@ -1,15 +1,25 @@
 package models;
 
 public class Album {
-    public static String genre;
+
+    public String genre;
     public String artist;
     public String title;
     public int release_year;
     public int price;
 
+    public Album(String genre, String artist, String title, int release_year, int price ) {
+        this.genre = genre;
+        this.artist = artist;
+        this.title = title;
+        this.release_year = release_year;
+        this.price = price;
+
+    }
+
 
     public boolean musicPick(String userPick){
-        return (genre == userPick);
+        return (userPick.equals(this.genre));
     }
 
 }
