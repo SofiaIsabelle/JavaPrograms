b2f442fe36e532542a9acf92c10493bd7c7dd54d
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-21 16:24:54 -0400

    For Statement to calculate interest as prime numbers

---
+++ calculateInterest/src/com/sofiaisabelle/Main.java
@@ -0,0 +1,52 @@
+package com.sofiaisabelle;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+//        System.out.println("10,000 at 2% interest =  " + calculateInterest(10000.0, 2.0));
+//        System.out.println("10,000 at 3% interest =  " + calculateInterest(10000.0, 3.0));
+//        System.out.println("10,000 at 4% interest =  " + calculateInterest(10000.0, 4.0));
+//        System.out.println("10,000 at 5% interest =  " + calculateInterest(10000.0, 5.0));
+
+
+        for (int i = 2; i <= 8; i++) {
+            System.out.println("10,000 at " + i + "% interest =  " + calculateInterest(10000.0, i));
+        }
+
+        System.out.println("******************************");
+
+        for (int i = 8; i >= 2; i--) {
+            System.out.println("10,000 at " + i + "% interest =  " + calculateInterest(10000.0, i));
+        }
+
+        int count = 0;
+        for(int i = 1; i <= 13; i++) {
+            if (isPrime(i)) {
+                count++;
+                System.out.println("Number " + i + " is a Prime Number.");
+
+                }
+            }
+        }
+
+
+    public static boolean isPrime(int n){
+        if(n == 1){
+            return false;
+        }
+        for(int i = 2; i <= n/2; i++){
+            if(n % i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+
+    public static double calculateInterest(double amount, double interestRate){
+        return(amount * interestRate/100);
+    }
+
+}
