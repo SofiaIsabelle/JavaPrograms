f58ced0ca2cd5bc0dd6f52e6453df9083647e082
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-23 13:59:42 -0400

    Inheritance Part I

---
+++ Inheritance/src/com/sofiaisabelle/Main.java
@@ -0,0 +1,13 @@
+package com.sofiaisabelle;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal("Animal", 1, 1, 5, 5 );
+        Dog dog = new Dog("Yorkie", 3, 20, 2, 4, 1,24, "long and silky");
+        dog.eat();
+
+
+
+    }
+}
