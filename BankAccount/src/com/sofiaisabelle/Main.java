9022a67f66e104bef2d792d25cd6103278d2d97b
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-25 23:07:31 -0400

    Pushing extras to Git

--- BankAccount/src/com/sofiaisabelle/Main.java
+++ BankAccount/src/com/sofiaisabelle/Main.java
@@ -8,7 +8,7 @@ public class Main {
 
         BankAccount sofiaAccount = new BankAccount();
 
-       // BankAccount sofiasAccount = new BankAccount("23948282901", 500000000, "Sofia Isabelle Rahimi", "sofiainvestments@gmail.com","571-294-9677" );
+
         System.out.println(sofiaAccount.getNumber());
         System.out.println(sofiaAccount.getBalance());
         sofiaAccount.deposit(500);
