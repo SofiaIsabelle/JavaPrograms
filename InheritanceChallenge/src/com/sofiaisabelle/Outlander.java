b46801db976123947ed04accbfdfc324588757e3
Nina Rahimi <nina.i.rahimi@gmail.com>
2018-05-29 18:18:13 -0400

    More on Inheritance

--- InheritanceChallenge/src/com/sofiaisabelle/Outlander.java
+++ InheritanceChallenge/src/com/sofiaisabelle/Outlander.java
@@ -5,8 +5,30 @@ public class Outlander extends Car{
 
 
     // Command + n to generate constructor for the roadService , and you'l see that it initializes all the other fields that belong to the car class as well
-    public Outlander( int wheels, int doors, int gears, boolean isManual, int roadService) {
+    public Outlander(int roadService) {
         super("Outlander", "4WD", 5, 5, 6, false);
         this.roadService = roadService;
     }
+
+    public void accelerate(int rate){
+
+        int newVelocity = getCurrentVelocity() + rate;
+        if(newVelocity == 0){
+            stop();
+            changeGear(1);
+        }else if(newVelocity > 0 && newVelocity <= 10){
+            changeGear(1);
+        }else if(newVelocity > 10 && newVelocity <= 20){
+            changeGear(2);
+        }else if(newVelocity > 20 && newVelocity <= 30){
+            changeGear(3);
+        }else{
+            changeGear(4);
+        }
+
+        if(newVelocity > 0){
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
+    }
+
 }
